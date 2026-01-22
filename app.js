7ee578739d214c930ca4a47158f18692b5206fba
OLUPOT RAPHAEL <olupotraphael30@gmail.com>
2026-01-22 14:40:37 +0300

    Fix: Updated static HTML page

--- app.js
+++ app.js
@@ -12,10 +12,14 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
     res.send('My Week 2 API!');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.post('/user', (req, res) => {
@@ -25,16 +29,16 @@ app.post('/user', (req, res) => {
         return res.status(400).send('Error: Missing details in request body.');
     }
    
-    res.send('Hello, ${name}!');
+    res.send(`Hello, ${name}, we received a request from ${email}`);
 });
 
 app.get('/user/:id', (req, res) => {
     const userId = req.params.id;
-    res.send('User ${userId} profile');
+    res.send(`User ${userId} profile`);
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:${PORT}');
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 
